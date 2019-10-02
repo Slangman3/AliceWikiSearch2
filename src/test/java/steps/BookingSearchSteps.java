3586d90ed85c0f4735adcd9d69f68314f0360065
Slangman3 <slangman3@gmail.com>
2019-10-02 20:01:30 +0300

    added allureUtils for scrshots and set Steps

--- src/test/java/steps/BookingSearchSteps.java
+++ src/test/java/steps/BookingSearchSteps.java
@@ -1,5 +1,6 @@
 package steps;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.BookingMainPage;
@@ -16,11 +17,13 @@ public class BookingSearchSteps {
     private static final String Booking_URL = "https://www.booking.com/index.ru.html";
 
     @cucumber.api.java.en.Given("Input keyword for searching {string}")
+    @Step("Ввод ключевого слова {string} в строку поиска")
     public void searchKeywordIsString(String keyword) {
         searchItem = keyword;
     }
 
     @cucumber.api.java.en.When("User does search on Booking")
+    @Step("Заходим на сайт Букинга")
     public void search() {
         driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         driver.get(Booking_URL);
@@ -32,11 +35,13 @@ public class BookingSearchSteps {
     }
 
     @cucumber.api.java.en.Then("Booking shows {string}")
+    @Step("Загрузка страницы результата поиска")
     public void bookingShows(String hotelsResult) {
         searchPage.resultPageShouldContainHotel(hotelsResult);
     }
 
     @cucumber.api.java.en.And("rating should be {string}")
+    @Step("Проверка наличия отеля и его рейтинга {string}")
     public void ratingShouldBe(String ratingResult) {
         String hotelName = searchItem;
         searchPage.hotelShouldBeRated(hotelName, ratingResult);
